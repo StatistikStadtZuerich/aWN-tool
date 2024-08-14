80c3310bc0e26e1ae1ea8ce476ac3591afd613d0
Easwaralingam Thamilini (SSZ) <Thamilini.Easwaralingam@zuerich.ch>
2024-08-14 17:59:29 +0200

    Sort addresses alphabetically by street name and numerically by house number in data preparation script. To ensure correct ordering in Shiny app dropdown menu.

--- R/utils_dataload_datatransformation.R
+++ R/utils_dataload_datatransformation.R
@@ -6,149 +6,125 @@
 #' 
 #' @return a named list of tibbles with zones, series, and addresses
 #' @noRd
+#' 
+#' 
+library(data.table)#package were not loaded, need to update these two into the script awn-tool package.R
+library(furrr)
 get_data <- function() {
-  
-  # Applying tryCatch
   tryCatch(
-    expr = { # Specifying expression
-      # By default the data frame is empty
+    expr = {
       data <- NULL
       
       ## URLS
       URLs <- c(
-        "https://data.stadt-zuerich.ch/dataset/geo_gebaeude__und_wohnungsregister_der_stadt_zuerich__gwz__gemaess_gwr_datenmodell/download/gwr_stzh_gebaeude.csv", #gebäude
-        "https://data.stadt-zuerich.ch/dataset/geo_gebaeude__und_wohnungsregister_der_stadt_zuerich__gwz__gemaess_gwr_datenmodell/download/gwr_stzh_gebaeudeeingaenge.csv",#eingang
-        "https://data.stadt-zuerich.ch/dataset/geo_gebaeude__und_wohnungsregister_der_stadt_zuerich__gwz__gemaess_gwr_datenmodell/download/gwr_stzh_wohnungen.csv" #wohnung
-
+        "https://data.stadt-zuerich.ch/dataset/geo_gebaeude__und_wohnungsregister_der_stadt_zuerich__gwz__gemaess_gwr_datenmodell/download/gwr_stzh_gebaeude.csv",
+        "https://data.stadt-zuerich.ch/dataset/geo_gebaeude__und_wohnungsregister_der_stadt_zuerich__gwz__gemaess_gwr_datenmodell/download/gwr_stzh_gebaeudeeingaenge.csv",
+        "https://data.stadt-zuerich.ch/dataset/geo_gebaeude__und_wohnungsregister_der_stadt_zuerich__gwz__gemaess_gwr_datenmodell/download/gwr_stzh_wohnungen.csv"
       )
       
       ## Download
       dataDownload <- function(link) {
-        data <- fread(link, encoding = "UTF-8"
+        message("Downloading data from: ", link)
+        data <- tryCatch(
+          {
+            fread(link, encoding = "UTF-8")
+          },
+          error = function(e) {
+            message("Error downloading data from: ", link)
+            message("Error details: ", e)
+            NULL
+          }
         )
-        
         return(data)
       }
       
       # Parallelisation
       data <- future_map(URLs, dataDownload)
-    },
-    error = function(e) { # Specifying error message
-      message("Error in Data Load")
+      
+      # Check if any data download failed
+      if (any(sapply(data, is.null))) {
+        stop("One or more datasets failed to download.")
+      }
+      
+      if (!is.null(data)) {
+        ### Data Transformation
+        
+        # Assuming the structure of data corresponds to the downloaded datasets
+        gebaeude <- data[[1]]  # First dataset
+        eingang <- data[[2]]   # Second dataset
+        wohnung <- data[[3]]   # Third dataset
+        
+        # Filter based on yellow-highlighted variables 
+        filtered_gebaeude <- gebaeude %>%
+          filter(GSTATLang == "Bestehend",
+                 GDEKT == "ZH",
+                 GGDENR == 261,
+                 GGDENAME == "Zürich")  
+        
+        # Select and transform the data needed for building infroamtion
+        building_with_address <- filtered_gebaeude %>%
+          left_join(eingang, by = "EGID") %>%  # Joining by EGID to get address data
+          select(
+            EGID,                # Unique building ID
+            STRNAME,             # Street Name
+            DEINR,               # House Number
+            DPLZ4,               # Postal Code
+            DPLZNAME,            # City
+            GBAUJ,               # Building year
+            GKLASLang,           # Building class
+            GASTW,               # Number of floors
+            GAZZI,               # Underground floors
+            GSCHUTZRLang,        # Civil protection room
+            GWAERZH1Lang,        # Heating system 1
+            GENH1Lang,           # Energy source 1
+            GWAERZW1Lang,        # Warm water generator 1
+            GENW1Lang            # Energy source warm water 1
+          ) %>%
+          mutate(DEINR_numeric = as.numeric(gsub("\\D", "", DEINR)),
+            Address = paste(STRNAME, DEINR, DPLZ4, DPLZNAME, sep = " ")  # Create a full address
+          ) %>% 
+          unique() %>%
+          arrange(STRNAME, DEINR_numeric, DEINR)
+        
+        # Select and transform the data for apartments
+        transformed_apartments <- wohnung %>%
+          select(
+            EGID,                # Unique building ID (to join with building)
+            WHGNR,               # Apartment number
+            EWID,                # Apartment ID
+            WBEZ,                # location of the apartement
+            WSTWKLang,           # Floor
+            WAZIM,               # Number of rooms
+            WAREA,               # Living space in m2
+            WKCHELang,           # is there kitchen facility
+            STRNAME,             # Street Name
+            DEINR,               # House Number
+            DPLZ4,               # Postal Code
+            DPLZNAME             # City
+            ) %>%
+              mutate(DEINR_numeric = as.numeric(gsub("\\D", "", DEINR)),
+                Address = paste(STRNAME, DEINR, DPLZ4, DPLZNAME, sep = " ")  # Create a full address
+              ) %>%
+          unique() %>%
+          arrange(STRNAME, DEINR_numeric, DEINR)
+        
+        # Return the final transformed data
+        return(list(
+          building_info = building_with_address,
+          apartment_info = transformed_apartments
+        ))
+      }
+      
+    }, # Closing expr block
+    error = function(e) {
+      message("Error in Data Load: ", e)
       return(NULL)
     },
-    warning = function(w) { # Specifying warning message
-      message("Warning in Data Load")
+    warning = function(w) {
+      message("Warning in Data Load: ", w)
     }
-  )
-  
-  if (!is.null(data)) {
-    ### Data Transformation
-    
-    ## Zones
-    zones <- data[[1]] %>%
-      mutate(PreisreiheLang = case_when(PreisreiheSort == 41 ~ "Preis pro m\u00B2 Grundstücksfläche",
-                                        PreisreiheSort == 42 ~ "Preis pro m\u00B2 Grundstücksfläche, abzgl. Versicherungswert",
-                                        PreisreiheSort == 49 ~ "Stockwerkeigentum pro m\u00B2 Wohnungsfläche")) %>%
-      mutate(across(everything(), \(x) replace(x, x == ".", "–")))  %>%
-      mutate(across(everything(), \(x) replace(x, x == "", "–"))) %>% 
-      mutate(ArtLang = case_when(
-        ArtLang == "Ganze Liegenschaft" ~ "Ganze Liegenschaften",
-        TRUE ~ ArtLang))
-    
-    ## BZO16
-    zonesBZO16 <- zones %>%
-      filter(BZO == "BZO16") %>%
-      rename(
-        Total = ALLE,
-        Z = ZE,
-        K = KE,
-        Q = QU,
-        W2 = W2,
-        W3 = W23,
-        W4 = W34,
-        W5 = W45,
-        W6 = W56
-      ) %>%
-      mutate(across(everything(), \(x) replace(x, x == ".", "–")))  %>%
-      mutate(across(everything(), \(x) replace(x, x == "", "–")))
-    
-    ## BZO99
-    zonesBZO99 <- zones %>%
-      filter(BZO == "BZO99") %>%
-      rename(
-        Total = ALLE,
-        Z = ZE,
-        K = KE,
-        Q = QU,
-        ` ` = W2,
-        W2 = W23,
-        W3 = W34,
-        W4 = W45,
-        W5 = W56
-      ) %>%
-      mutate(across(everything(), \(x) replace(x, x == ".", "–")))  %>%
-      mutate(across(everything(), \(x) replace(x, x == "", "–")))
-    
-    ## Series
-    series <- data[[2]] %>%
-      mutate(across(everything(), \(x) replace(x, x == ".", "–"))) %>%
-      mutate(across(c(
-        FrQmBodenGanzeLieg,
-        FrQmBodenStwE,
-        FrQmBodenAlleHA
-      ), \(x) replace(x, x == "", "–"))
-      )
-    
-    ## Addresses
-    addresses <- data[[3]] %>%
-      mutate(Zones = case_when(
-        ZoneBZO16Lang == ZoneBZO99Lang ~ paste(ZoneBZO16Lang),
-        TRUE ~ paste0(ZoneBZO16Lang, " (bis 2018: ", ZoneBZO99Lang, ")")
-      )) %>% 
-      mutate(ZoneBZO99Lang = case_when(
-        ZoneBZO99Lang == "Wohnzone 2" ~ "Wohnzonen 2",
-        ZoneBZO99Lang == "Wohnzone 3" ~ "Wohnzonen 3",
-        ZoneBZO99Lang == "Wohnzone 4" ~ "Wohnzonen 4",
-        ZoneBZO99Lang == "Wohnzone 5" ~ "Wohnzonen 5",
-        TRUE ~ ZoneBZO99Lang
-      ))
-    
-    
-    ## Building Type
-    types <- data[[4]] %>%
-      mutate(PreisreiheLang = case_when(PreisreiheSort == 41 ~ "Preis pro m\u00B2 Grundstücksfläche",
-                                        PreisreiheSort == 42 ~ "Preis pro m\u00B2 Grundstücksfläche, abzgl. Versicherungswert",
-                                        PreisreiheSort == 49 ~ "Stockwerkeigentum pro m\u00B2 Wohnungsfläche")) %>%
-      mutate(ArtLang = case_when(ArtSort == 31 ~ "Ganze Liegenschaften",
-                                 ArtSort == 32 ~ "Nur Stockwerkeigentum",
-                                 ArtSort == 39 ~ "Alle Handänderungen")) %>%
-      mutate(across(everything(), \(x) replace(x, x == ".", "–")))  %>%
-      mutate(across(everything(), \(x) replace(x, x == "", "–")))
-    
-    
-    ## Series Building Type
-    seriestypes <- data[[5]] %>%
-      mutate(across(everything(), \(x) replace(x, x == ".", "–"))) %>%
-      mutate(across(c(
-        FrQmBodenGanzeLieg,
-        FrQmBodenStwE,
-        FrQmBodenAlleHA
-      ), \(x) replace(x, x == "", "–"))
-      )
-    
-    return(list(
-      zones = zones,
-      zonesBZO16 = zonesBZO16,
-      zonesBZO99 = zonesBZO99,
-      series = series,
-      addresses = addresses,
-      types = types,
-      seriestypes = seriestypes
-    ))
-    
-  }
-}
-
+  ) # Closing tryCatch block
+} # Closing get_data function
 
 data_vector <- get_data()
+
