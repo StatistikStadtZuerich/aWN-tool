e9664ccf579e185484af0bd7401f437fa552eeff
Feller Alessandro (SSZ) <Alessandro.Feller@zuerich.ch>
2024-09-02 17:23:41 +0200

    Included CSS (not yet with golem)

--- R/app_ui.R
+++ R/app_ui.R
@@ -6,37 +6,42 @@
 #' @noRd
 app_ui <- function(request) {
   
-  data <- get_data()
   tagList(
     # External resources (e.g., CSS, JS)
     golem_add_external_resources(),
     
     # Page layout
     fluidPage(
-      titlePanel("Gebäude Information"),
+      
+      # TEMPORARY: until golem_add_external_resources() works
+      includeCSS("inst/app/www/sszThemeShiny.css"),
+      
+      tags$div(
+        class = "queryDiv",
+        h1("Wählen Sie eine Adresse"),
+        p("Mit dieser Applikation können Sie Abfragen zu den amtlichen Wohnungsnummern in der Stadt Zürich machen."),
+        hr()
+      ),
+      
       
       # Sidebar layout
       sidebarLayout(
         # Sidebar with inputs
         sidebarPanel(
           mod_input_ui("input_module")  # Address input module
-          
         ),
         
         # Main panel for outputs
         mainPanel(
-          h3("Adresse eingeben"),
-          p("Mit dieser Applikation können Sie Abfragen zu den amtlichen Wohnungsnummern in der Stadt Zürich machen."),
-          hr(),
           
-          # UI outputs for building, entrance, and apartment information
-          h4("Informationen zum Gebäude"),
+          # tbd: call mod_result functions !!!!!
+          #mod_building_ui(id "asdf")
+          
+          # TEMPORARY OUTPUTS BEFORE MODULARIZATION
           uiOutput("building_info"),
           
-          h4("Heizung & Wasser"),
           uiOutput("entrance_info"),
           
-          h4("Informationen zu Wohnungen"),
           uiOutput("apartment_info"),
           tableOutput("apartment_table")
         )
@@ -44,29 +49,30 @@ app_ui <- function(request) {
     )
   )
 }
-
-#' Add external Resources to the Application
-#'
-#' This function is internally used to add external
-#' resources inside the Shiny application.
-#'
-#' @import shiny
-#' @importFrom golem add_resource_path activate_js favicon bundle_resources
-#' @noRd
-golem_add_external_resources <- function() {
-  add_resource_path(
-    "www",
-    app_sys("app/www")
-  )
   
-  tags$head(
-    favicon(),
-    bundle_resources(
-      path = app_sys("app/www"),
-      app_title = "awntool"
-    ),
-    # Example for adding ShinyJS (if needed)
-    shinyjs::useShinyjs(debug = TRUE)
-  )
-}
-
+  #' Add external Resources to the Application
+  #'
+  #' This function is internally used to add external
+  #' resources inside the Shiny application.
+  #'
+  #' @import shiny
+  #' @importFrom golem add_resource_path activate_js favicon bundle_resources
+  #' @noRd
+  golem_add_external_resources <- function() {
+    add_resource_path(
+      "www",
+      app_sys("app/www")
+    )
+    
+    tags$head(
+      favicon(),
+      bundle_resources(
+        path = app_sys("app/www"),
+        app_title = "aWNtool"
+      ),
+      # Example for adding ShinyJS (if needed)
+      shinyjs::useShinyjs(debug = TRUE)
+    )
+  }
+  
+  
