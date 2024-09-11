d43c92b34cfed7cb560333035670241ab9ba27d3
Easwaralingam Thamilini (SSZ) <Thamilini.Easwaralingam@zuerich.ch>
2024-09-11 17:27:22 +0200

    Added additional variables in the taglist of the cards

--- R/fct_get_card.R
+++ R/fct_get_card.R
@@ -29,10 +29,15 @@ get_card <- function(dataset, height, card_min_height, card_width) {
         ),
         tagList(
           h4("Heizung & Wasser"),
-          p(paste("Wärmeerzeuger Heizung:", dataset$GWAERZH1Lang)),
-          p(paste("Energiequelle Heizung:", dataset$GENH1Lang)),
-          p(paste("Wärmeerzeuger Warmwasser:", dataset$GWAERZW1Lang)),
-          p(paste("Energiequelle Warmwasser:", dataset$GENW1Lang))
+          p(paste("Wärmeerzeuger Heizung 1:", dataset$GWAERZH1Lang)),
+          p(paste("Energiequelle Heizung 1:", dataset$GENH1Lang)),
+          p(paste("Wärmeerzeuger Heizung 2:", dataset$GWAERZH2Lang)),
+          p(paste("Energiequelle Heizung 2:", dataset$GENH2Lang)),
+          p(paste("Wärmeerzeuger Warmwasser 1:", dataset$GWAERZW1Lang)),
+          p(paste("Energiequelle Warmwasser 1:", dataset$GENW1Lang)),
+          p(paste("Wärmeerzeuger Warmwasser 2:", dataset$GWAERZW2Lang)),
+          p(paste("Energiequelle Warmwasser 2:", dataset$GENW2Lang))
+          
         )
       )
     )
