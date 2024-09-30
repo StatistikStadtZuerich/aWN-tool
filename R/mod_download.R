a6fe941c0d4d5d42da2b591dca718aab71e1b875
Feller Alessandro (SSZ) <Alessandro.Feller@zuerich.ch>
2024-09-30 18:08:02 +0200

    Nicer Download Output

--- R/mod_download.R
+++ R/mod_download.R
@@ -43,64 +43,69 @@ mod_download_ui <- function(id) {
 #' @param building_data The filtered building data to be downloaded.
 #' @param apartment_data The filtered apartment data to be downloaded.
 #' @noRd 
-mod_download_server <- function(id, building_data, apartment_data) {
+mod_download_server <- function(id, building_data, apartment_data, fct_create_excel) {
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
     
     # Prepare the data for download, filtering based on the displayed content
     data_for_download <- reactive({
-      building_info <- building_data() %>%
-        select(Address, EGID, GKLASLang, GBAUJ, GASTW, GAZZI, GSCHUTZRLang, GWAERZH1Lang, GENH1Lang, GWAERZW1Lang, GENW1Lang)
       
+      # Data with Building Infos
+      building_info <- building_data() %>%
+        select(Address, EGID, GKLASLang, GBAUJ, GASTW, GAZZI, GSCHUTZRLang, GWAERZH1Lang, GENH1Lang, GWAERZW1Lang, GENW1Lang) |> 
+        rename(
+          `Adresse` = Address,
+          `Gebäudetyp` = GKLASLang,
+          `Baujahr` = GBAUJ,
+          `Oberirdische Geschosse` = GASTW,
+          `Unterirdische Geschosse` = GAZZI,
+          `Zivilschutzraum` = GSCHUTZRLang,
+          `Wärmeerzeuger Heizung 1` = GWAERZH1Lang,
+          `Energiequelle Heizung 1` = GENH1Lang,
+          `Wärmeerzeuger Warmwasser 1` = GWAERZW1Lang,
+          `Energiequelle Warmwasser 1` = GENW1Lang
+        )
+      
+      # Data with Aparment Infos
       apartment_info <- apartment_data() %>%
-        select(WHGNR, EWID, WSTWKLang, WAZIM, WAREA, WKCHELang)
+        select(Address, WHGNR, EWID, WSTWKLang, WBEZ, WAZIM, WAREA, WKCHELang) |> 
+        rename(
+          `Adresse` = Address,
+          `aWN` = WHGNR,
+          `EWID` = EWID,
+          `Stockwerk` = WSTWKLang,
+          `Lage Wohnung` = WBEZ,
+          `Zimmer` = WAZIM,
+          `Wohnfläche (m2)` = WAREA,
+          `Küche` = WKCHELang
+        )
+      
+      # Number of Aparments
+      number_aparments <- nrow(apartment_info)
+      
+      # Address
+      address <- building_data()$Address[1]
       
       # Combine the building and apartment data
       list(
         Building_Info = building_info,
-        Apartment_Info = apartment_info
+        Apartment_Info = apartment_info,
+        Address = address,
+        Number_Aparments = number_aparments
       )
     })
-    
-    # Get the selected address to use in the filename
-    address_for_filename <- reactive({
-      address <- building_data()$Address[1]  # Get the selected address
-      gsub(" ", "_", address) 
-    })
-    
-    # CSV Download
-    # output$csv_download <- downloadHandler(
-    #   filename = function() {
-    #     paste0(address_for_filename(), "_", Sys.Date(), ".csv")
-    #   },
-    #   content = function(file) {
-    #     data_to_save <- data_for_download()$Building_Info  # Get the building info
-    #     write.csv(data_to_save, file, row.names = FALSE, na = "")
-    #   }
-    # )
     
     # Excel Download
     output$excel_download <- downloadHandler(
       filename = function() {
-        paste0(address_for_filename(), "_", Sys.Date(), ".xlsx")
+        # Use the address from data_for_download() and replace spaces with underscores
+        address <- gsub(" ", "_", data_for_download()$Address)
+        paste0("Gebäude_und_Wohnungsinformationen_", address, "_", Sys.Date(), ".xlsx")
       },
       
       content = function(file) {
-        
-        # Get the building and apartment data
-        building_info <- data_for_download()$Building_Info  
-        apartment_info <- data_for_download()$Apartment_Info  
-        
-        # Check if apartment data has rows
-        if (nrow(apartment_data()) > 0) {
-          # Both building and apartment data available
-          writexl::write_xlsx(list(Gebäudeinfos = building_info, 
-                                   Wohnungsinfos = apartment_info), 
-                              file)
-        } else {
-          # Only building data available
-          writexl::write_xlsx(list(Gebäudeinfos = building_info), file)
-        }
+        fct_create_excel(file, data_for_download())
+
       }
     )
   })
