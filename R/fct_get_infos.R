4c87d2a97083a6c10a6c05252df47d5cd063f043
Feller Alessandro (SSZ) <Alessandro.Feller@zuerich.ch>
2024-10-16 18:30:01 +0200

    New function for div with warning for invalid addresses

--- R/fct_get_infos.R
+++ R/fct_get_infos.R
@@ -1,4 +1,4 @@
-#' Info Div when for Buildings with no Appartments
+#' Info Div for Buildings with no Appartments
 #'
 #' @description Function to create a Warning div
 #'
@@ -10,6 +10,33 @@
 #' @noRd
 get_na_info <- function(title = "Info",
                         text = "In diesem Gebäude gibt es keine Wohnungen.") {
+  ssz_icons <- icons::icon_set("inst/app/www/icons/")
+  tags$div(
+    class = "info_na_div",
+    tags$div(
+      class = "info_na_icon",
+      img(ssz_icons$`warning`)
+    ),
+    tags$div(
+      class = "info_na_text",
+      h6(title),
+      p(text)
+    )
+  )
+}
+
+#' Info Div for invalid addresses
+#'
+#' @description Function to create a Warning div
+#'
+#' @param title Title that should appear in the block
+#' @param text Text that should appear in the block
+#'
+#' @return Div
+#'
+#' @noRd
+get_warning <- function(title = "Warnung",
+                        text = "Die eingegebene Adresse existiert nicht.") {
   ssz_icons <- icons::icon_set("inst/app/www/icons/")
   tags$div(
     class = "info_na_div",
