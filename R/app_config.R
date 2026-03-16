9acbfa76ae039da897795b9ac6234b0ee8c69748
Feller Alessandro (SSZ) <Alessandro.Feller@zuerich.ch>
2026-03-16 20:10:55 +0100

    ssz_icon function in the config

--- R/app_config.R
+++ R/app_config.R
@@ -12,6 +12,12 @@ app_sys <- function(...) {
   system.file(..., package = "awntool")
 }
 
+#' Load SSZ icons
+#' @noRd
+ssz_icons <- function() {
+  icons::icon_set(here::here("inst/app/www/icons/"))
+}
+
 
 #' Read App Config
 #'
