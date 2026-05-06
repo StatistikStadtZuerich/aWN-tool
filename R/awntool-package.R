d1641e84d3fb6b6e505a598f08cf94bbe27e3696
Hänzi Sara (SSZ) <sara.haenzi@zuerich.ch>
2026-05-06 14:20:49 +0200

    fix namespace import clash between shiny and shinyjs

--- R/awntool-package.R
+++ R/awntool-package.R
@@ -3,22 +3,22 @@
 
 ## usethis namespace: start
 #' @import bslib
+#' @import dplyr
+#' @import jpeg
+#' @import openxlsx
+#' @import purrr
+#' @import reactable
+#' @import shiny
+#' @import zuericssstyle
 #' @importFrom data.table fread
 #' @importFrom furrr future_map
-#' @import purrr
-#' @import dplyr
 #' @importFrom golem add_resource_path activate_js favicon bundle_resources
 #' @importFrom gtools mixedsort
-#' @import jpeg
 #' @importFrom kableExtra kable_styling
 #' @importFrom knitr kable
-#' @import openxlsx
-#' @import reactable
 #' @importFrom readxl read_excel
-#' @import shiny
 #' @importFrom shinycssloaders withSpinner
-#' @import shinyjs
+#' @importFrom shinyjs hide hidden show useShinyjs
 #' @importFrom tidyr pivot_longer
-#' @import zuericssstyle
 ## usethis namespace: end
 NULL
