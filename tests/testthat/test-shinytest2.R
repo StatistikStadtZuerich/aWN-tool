6ef4b118b6634788bb072fec816b5b08e6dec176
Hänzi Sara (SSZ) <sara.haenzi@zuerich.ch>
2026-05-06 14:23:55 +0200

    get shinytest to work on workbench

--- tests/testthat/test-shinytest2.R
+++ tests/testthat/test-shinytest2.R
@@ -1,11 +1,5 @@
 library(shinytest2)
 
-# Setup any PROXY setting here
-http_proxy <- Sys.getenv("HTTP_PROXY")
-Sys.setenv("HTTP_PROXY" = "")
-chromote::set_chrome_args(paste0("--http-proxy=", http_proxy))
-
-
 test_that("{shinytest2} recording: awn-tool starting values", {
   app <- AppDriver$new(name = "awn-tool", height = 953, width = 1619)
   app$set_inputs(`input_module-address` = "Verenastrasse 16")
@@ -16,7 +10,6 @@ test_that("{shinytest2} recording: awn-tool starting values", {
                     export = TRUE
   )
 })
-
 
 test_that("{shinytest2} recording: test_sorting_floor_correctly", {
   app <- AppDriver$new(name = "test_sorting_floor_correctly", height = 953, width = 1619)
@@ -39,7 +32,6 @@ test_that("{shinytest2} recording: test_abc_wohnungen", {
                     output = FALSE, 
                     export = TRUE)
 })
-
 
 test_that("{shinytest2} recording: ungültige Strasse", {
   app <- AppDriver$new(name = "ungültige Strasse", height = 1073, width = 1619)
